7542de4de2fe5ebba8057b51c9f35adeb717b829
Vaishnavi Kanchan <vaishnavibhaskar.kanchan@srh-heidelberg.org>
2024-11-09 00:35:30 +0000

    initial commit

---
+++ models/example/transformations.sql
@@ -0,0 +1,41 @@
+---we got the latest video id information, excluded the irrelevant columns replaced null values
+WITH initial_ranking AS (
+    SELECT 
+        *,
+        ROW_NUMBER() OVER (PARTITION BY channel_id ORDER BY total_views DESC) AS row_num
+    FROM {{ source('youtube_data', 'pubtogcs') }}
+),
+filtered_data AS (
+    SELECT 
+        title,
+        video_id,
+        channel_id,
+        channel_title,
+        total_views,
+        COALESCE(likes, 0) AS likes,
+        COALESCE(dislikes, 0) AS dislikes,
+        COALESCE(comments, 0) AS comments,
+        COALESCE(category, 'unknown') AS category
+    FROM initial_ranking
+    WHERE row_num = 1
+),
+final_ranking AS (
+    SELECT 
+        * ,
+        ROW_NUMBER() OVER (PARTITION BY channel_id ORDER BY total_views DESC) AS row_num
+    FROM filtered_data
+)
+SELECT *
+FROM final_ranking
+WHERE row_num = 1
+AND (
+    title IS NOT NULL
+    OR video_id IS NOT NULL
+    OR channel_id IS NOT NULL
+    OR channel_title IS NOT NULL
+    OR total_views IS NOT NULL
+    OR likes IS NOT NULL
+    OR dislikes IS NOT NULL
+    OR comments IS NOT NULL
+    OR category IS NOT NULL
+)
